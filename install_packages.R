8f45765d1b5017bc48313e7280249fbec9c992db
Ravan Nannapaneni <ravan_n@hotmail.com>
2018-08-05 16:51:13 +0800

    Started wrangling

---
+++ install_packages.R
@@ -0,0 +1,7 @@
+install.packages('ggplot2', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+install.packages('tidyverse', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+install.packages('dslabs', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+install.packages('readr', dependencies=TRUE, repos='http://cran.rstudio.com/')
