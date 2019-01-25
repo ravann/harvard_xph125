248a6ad888567967c99acd19426c49e12202aa5f
Ravan Nannapaneni <ravan.nannapaneni@oliverwyman.com>
2019-01-25 13:32:48 +0800

    Added new packages ...

--- install_packages.R
+++ install_packages.R
@@ -1,3 +1,5 @@
+system('defaults write org.R-project.R force.LANG en_US.UTF-8')
+
 install.packages('ggplot2', dependencies=TRUE, repos='http://cran.rstudio.com/')
 
 install.packages('tidyverse', dependencies=TRUE, repos='http://cran.rstudio.com/')
@@ -5,3 +7,11 @@ install.packages('tidyverse', dependencies=TRUE, repos='http://cran.rstudio.com/
 install.packages('dslabs', dependencies=TRUE, repos='http://cran.rstudio.com/')
 
 install.packages('readr', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+install.packages('gapminder', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+install.packages('stringr', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+install.packages('Lahman', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+install.packages('HistData', dependencies=TRUE, repos='http://cran.rstudio.com/')
